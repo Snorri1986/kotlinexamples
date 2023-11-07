934ded5328f5ef8652efc19982316495a34941ee
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-07 21:50:15 +0100

    first touch user input

---
+++ KotlinUdemyDemos/src/main/kotlin/Inputs.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name:String = readln()
+    val age: Int = readln().toInt()
+    val GPA:Double = readln().toDouble()
+
+    println("======= User info ===========")
+    println("Name $name")
+    println("Age $age")
+    println("GPA $GPA")
+}
